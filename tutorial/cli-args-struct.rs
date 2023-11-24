3d056ea7a39fa0b035cc19e49fafdf5aacc3f873
getong <getong@users.noreply.github.com>
2023-11-24 21:49:14 +0000

    deploy: c35663f29852f5da830cacff671171bc3ffa9849

--- tutorial/cli-args-struct.rs
+++ tutorial/cli-args-struct.rs
@@ -1,17 +1,16 @@
-#![allow(unused)]
-
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
 
-let pattern = std::env::args().nth(1).expect("no pattern given");
-let path = std::env::args().nth(2).expect("no path given");
-let args = Cli {
-    pattern: pattern,
-    path: std::path::PathBuf::from(path),
-};
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    };
 
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
--- tutorial/errors-impl.rs
+++ tutorial/errors-impl.rs
@@ -12,25 +12,15 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
+
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
-
-    Ok(())
-}
-
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
+        if line.contains(&args.pattern) {
+            println!("{}", line);
         }
     }
-}
 
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
+    Ok(())
 }
